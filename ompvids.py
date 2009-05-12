9ec63ef093d76a180176389f596d57e42d15120e
Brenden Matthews <brenden@rty.ca>
2009-05-12 14:18:30 -0600

    New files, gitignore.

---
+++ ompvids.py
@@ -0,0 +1,47 @@
+#
+# This is common stuff for the ompvids scripts.
+#
+
+import boto
+from boto.s3.key import Key
+import sha
+import os
+import re
+
+bucket_name = os.environ['AWS_BUCKET'] # will assplode if not defined in environment
+passkey = os.environ['OMPVIDS_PASSKEY'] # will assplode if not defined in environment
+			
+host = 'localhost'
+port = 50000
+size = 4096
+max_wait = 30 # seconds
+min_wait = 5 # seconds
+
+fn_exp = re.compile('([A-Za-z0-9]+)-(.+)')
+key_exp = re.compile('([A-Za-z0-9]+)/(.+)')
+
+def filename_to_key(filename):
+	res = fn_exp.match(filename)
+	if not res:
+		return
+	id = res.group(1)
+	name = res.group(2)
+	key = id + '/' + name
+	return key
+
+def key_to_filename(key):
+	res = key_exp.match(key)
+	if not res:
+		return
+	id = res.group(1)
+	name = res.group(2)
+	filename = id + '-' + name
+	return filename
+
+def get_bucket():
+	conn = boto.connect_s3()
+	return conn.create_bucket(bucket_name)
+
+
+def get_answer(passkey, challenge):
+	return sha.new('%s %li' % (passkey, challenge)).hexdigest()
