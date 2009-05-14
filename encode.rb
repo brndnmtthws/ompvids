97c6f83a4adcf888c0d84afd5271a7435fd0a633
Javeed Shaikh <syscrash2k@gmail.com>
2009-05-14 17:51:54 -0400

    don't needlessly re-encode ogg+theora+vorbis some preview timing and generation improvements

--- encode.rb
+++ encode.rb
@@ -4,12 +4,34 @@ require 'fileutils'
 
 THUMBWIDTH = 120
 THUMBHEIGHT = 90
-INPUT_FPS = 120
-PREVIEW_DURATION = 30
+
+# how much faster the input video should be played when generating the preview
+INPUT_SPEED_FACTOR = 6
+
+# how much of the input video we want to sample, as a fraction (0.0 = nothing, 1.0 = whole video)
+PREVIEW_DURATION = 1.0
+
+# the minimum number of seconds of the input video we want to sample
+PREVIEW_DURATION_FLOOR = 30
+
+# the framerate of the preview; increasing this will make the preview smoother
 PREVIEW_FPS = 5
+
 AUDIO_QUALITY = 5
 VIDEO_QUALITY = 7
 TMP_PATH = '/tmp'
+
+def video_info(filename)
+  f = IO.popen("mplayer -ao null -vo null -identify -endpos 0 '#{filename}'")
+  info = {}
+  f.each do |line|
+	if line =~ /^ID_(.+)=(.+)/
+	  info[$1] = $2
+	end
+  end
+  # pro method of determining whether this is a valid video
+  info['FILENAME'] == filename ? info : nil
+end
 
 # returns an array [width, height]
 def image_size(filename)
@@ -24,10 +46,11 @@ def image_size(filename)
   return [width, height]
 end
 
-def generate_preview(input, output, output_still, width)
+def generate_preview(input, input_fps, endpos, output, output_still, width)
   # mplayer is not very happy with spaces in the output file name, p awesome
-  system('mplayer', '-vf', 'scale', '-zoom', '-fps', INPUT_FPS.to_s, '-xy', THUMBWIDTH.to_s, '-benchmark', '-ao', 'null', '-endpos', PREVIEW_DURATION.to_s, '-vo', "gif89a:fps=#{PREVIEW_FPS}:output=\"#{TMP_PATH}/preview.gif\"", input) or return false
-  system('mogrify', '-layers', 'optimize', "#{TMP_PATH}/preview.gif") or return false
+  # this also uses ffmpegthumbnailer as a fallback in case the video is too short for mplayer to catch it (reach out a little bit more to catch it)
+  system('mplayer', '-vf', 'scale', '-zoom', '-fps', input_fps.to_s, '-xy', THUMBWIDTH.to_s, '-benchmark', '-ao', 'null', '-endpos', endpos.to_s, '-vo', "gif89a:fps=#{PREVIEW_FPS}:output=\"#{TMP_PATH}/preview.gif\"", input) or return false
+  system('mogrify', '-layers', 'optimize', "#{TMP_PATH}/preview.gif") or (system('ffmpegthumbnailer', '-s', THUMBWIDTH.to_s, '-i', input, '-o', "#{TMP_PATH}/preview.gif") and system('mogrify', '-layers', 'optimize', "#{TMP_PATH}/preview.gif")) or return false
   system('convert', '-coalesce', '-flatten', "#{TMP_PATH}/preview.gif", "#{TMP_PATH}/preview-still.gif") or return false
   FileUtils::mv("#{TMP_PATH}/preview.gif", output)
   FileUtils::mv("#{TMP_PATH}/preview-still.gif", output_still)
@@ -42,12 +65,27 @@ input = ARGV[0]
 if ARGV[1]
 	TMP_PATH = ARGV[1]
 end
+
 output = "#{input}.ogg"
 thumbnail = "#{input}.gif"
 thumbnail_still = "#{input}-still.gif"
 
-system('ffmpeg2theora', '--audioquality', AUDIO_QUALITY.to_s, '--videoquality', VIDEO_QUALITY.to_s, '--optimize', '-o', output, input) or exit 1
-generate_preview(input, thumbnail, thumbnail_still, THUMBWIDTH) or return 1
+(info = video_info(input)) or exit 1
+
+# don't re-encode if you don't have to
+if (info['DEMUXER'] != 'ogg') or (info['VIDEO_FORMAT'] != 'theo') or (info['AUDIO_FORMAT'] != 'vrbs')
+  system('ffmpeg2theora', '--audioquality', AUDIO_QUALITY.to_s, '--videoquality', VIDEO_QUALITY.to_s, '--optimize', '-o', output, input) or exit 1
+else
+  FileUtils::cp(input, output)
+end
+
+input_fps = info['VIDEO_FPS'].to_i * INPUT_SPEED_FACTOR
+endpos = info['LENGTH'].to_i * PREVIEW_DURATION
+# sometimes mplayer can't determine the input length, in which case we'll take 30 seconds
+if endpos < PREVIEW_DURATION_FLOOR
+  endpos = PREVIEW_DURATION_FLOOR
+end
+generate_preview(input, input_fps.to_i, endpos.to_i, thumbnail, thumbnail_still, THUMBWIDTH) or exit 1
 
 # does this thing fit in the thumbnail box?
 width, height = image_size thumbnail
